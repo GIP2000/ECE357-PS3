547b009c9f7bea7cd41e843839a0760ed254c8c6
Gregory Presser <gpress2222@gmail.com>
2021-10-18 17:13:29 -0400

    "changes"

--- shell.c
+++ shell.c
@@ -10,9 +10,9 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/types.h>
+#include <time.h> 
 
 void processIORedirect(char * ioRedirect){
-    fprintf(stderr,"ioRedirect: %s; len:%d; firstchar:%d\n",ioRedirect,strlen(ioRedirect),(int)ioRedirect[0]);  
     char * token = strtok(ioRedirect," ");
     do{
         int source = -1; 
@@ -23,13 +23,7 @@ void processIORedirect(char * ioRedirect){
             case '2': 
                 // redirect stderr
                 source = STDERR_FILENO; 
-                strcpy(token,token+1); // i didn't break so I can continue and just use the code from the next section cause its the same 
-                // flag = token[2] == '>' ? O_APPEND|O_CREAT|O_WRONLY : O_TRUNC|O_CREAT|O_WRONLY;
-                // int offset = token[2] == '>' ?3:2; 
-                // desName = malloc(strlne(token) - offset+1);
-                // strcpy(desName,token+offset);
-                // desName[strlne(token) - offset] = '\0'; 
-                // break; 
+                strcpy(token,token+1); // fall through
             case '>':
                 //redirect stdout
                 source = source == -1 ? STDOUT_FILENO: source; 
@@ -51,8 +45,8 @@ void processIORedirect(char * ioRedirect){
         }
         int fd = open(desName,flag,mode); 
         if(fd == -1){
-            fprintf(stderr,"Error opening file name \"%s\":%s\n",desName,strerror(errno)); 
-            exit(-1); 
+            fprintf(stderr,"Error opening file name \"%s\" when redirecting IO. Error:%s\n",desName,strerror(errno)); 
+            exit(1); 
         }
         dup2(fd,source); 
         free(desName); 
@@ -65,16 +59,17 @@ void execute(char * command, char * arguments){
     argv[0] = command;
     int i = 1;   
     char * token = strtok(arguments," ");
-    do{
+    while(token != NULL){
         currentSize+=sizeof(char*)*(strlen(token)) ;
         argv = (char**)realloc(argv,currentSize);
         argv[i++] = token; 
-    }while((token = strtok(NULL," ")) != NULL);
+        token = strtok(NULL," "); 
+    }
     argv[i] = NULL;
     execvp(command,argv); 
-    perror("execvp"); 
+    perror("execvp");
+    exit(127);  
 }
-
 void pwd(){
     int size = PATH_MAX == -1 || PATH_MAX > 10240 ? 10240:PATH_MAX;
     char * buf; 
@@ -93,12 +88,18 @@ void cd(char * directory, int len){
     else dir = strtok(directory, " ");
     if(chdir(dir) != 0) perror("Error Changing Directory: "); 
 }
-void exitShell(char* code){
-    // change to parse for the code; 
-    exit(atoi(code));
+void exitShell(char* arguments){
+    char * code = strtok(arguments," ");
+    code = code == NULL ? arguments:code; 
+    if(isdigit(code[0]) == 0 && !(isdigit(code[0]) == 0 && code[0] == '-' && strlen(code) > 1 && isdigit(code[1]) != 0))
+        fprintf(stderr, "Couldn't exit error code not an integer\n"); 
+    else exit(atoi(code));
 }
+
+// runs an external command
 void external(char* command, char* arguments, char* ioRedirect, bool hasRedirect){
     int pid; 
+    clock_t start = clock(); 
     switch((pid = fork())){
         case -1: perror("Error forking: "); exit(-1);
         case 0: {// child
@@ -113,32 +114,48 @@ void external(char* command, char* arguments, char* ioRedirect, bool hasRedirect
             if((cpid == wait3(&status,0,&ru)) == -1){
                 perror("Error waiting for child: ");
                 exit(-1);
-            } else {
-                printf("Child process %d consumed%ld.%.6d seconds of user time \n",
-                pid,
-                ru.ru_utime.tv_sec,
-                ru.ru_utime.tv_usec);
-            }
+            } 
+            clock_t total = (double)(clock() - start) / CLOCKS_PER_SEC; 
+
+            if(status != 0 ){
+                if (WIFSIGNALED(status))
+                    printf("Child process %d exited with signal %d\n", pid,WTERMSIG(status));
+                else 
+                    printf("Child process %d exited with return value %d\n", pid, WEXITSTATUS(status));
+            } else 
+                printf("Child process %d exited normally\n",pid); 
+            printf("Real:%fs User:%ld.%.6ds Sys:%ld.%.6ds\n",
+            total,
+            ru.ru_utime.tv_sec,
+            ru.ru_utime.tv_usec,
+            ru.ru_stime.tv_sec,
+            ru.ru_stime.tv_usec);
+            
         }
 
     }
 }
+
+// processes an input line 
 void processLine(char* line, int len){
     if(line[0] == '#') return;
     char* command;
     char* arguments; 
     char* ioRedirect; 
 
+    // gets the command 
     int commandIndex = strchr(line,' ') == NULL ? len-1:strchr(line,' ')-line ;
     command = (char *)malloc(commandIndex+1); 
     strncpy(command,line,commandIndex);
     command[commandIndex] = '\0';
 
+    // find the place where the args stop and the the io redirect starts
     int lessThanIndex = strchr(line,'<') == NULL ? len+1: strchr(line,'<')-line; 
     int greaterThanIndex = strchr(line,'>') == NULL ? len+1: strchr(line,'>')-line;  
     if(greaterThanIndex > 0 && line[greaterThanIndex-1] == '2') greaterThanIndex--; 
     int ioIndex = greaterThanIndex < lessThanIndex ? greaterThanIndex: lessThanIndex;
     
+    //seperates the args and the io redirect (if present )
     if(ioIndex != len+1) {
         arguments = malloc(ioIndex-(commandIndex+1)+1); 
         strncpy(arguments,line+commandIndex+1,ioIndex-(commandIndex+1));
@@ -152,6 +169,7 @@ void processLine(char* line, int len){
         arguments[len-(commandIndex+1)-1] = '\0';
     }
 
+    // determnes if I should use an inbuilt command or not 
     if(strcmp(command,"cd") == 0) cd(arguments,len-(commandIndex+1));
     else if(strcmp(command,"pwd") == 0) pwd(); 
     else if(strcmp(command,"exit") == 0) exitShell(arguments); 
@@ -162,6 +180,7 @@ void processLine(char* line, int len){
     else if(len-(commandIndex+1) != -1) free(arguments); 
 }
 
+// processes the input file
 void parseShell(FILE* fp){
     char * line = NULL; 
     size_t len = 0; 
@@ -173,5 +192,4 @@ void parseShell(FILE* fp){
 int main(int argc, char* argv[]){
     FILE* fp = argc == 1? stdin : fopen(argv[1],"r");
     parseShell(fp); 
-
 }
